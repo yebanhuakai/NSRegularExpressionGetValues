1d5654fefc84d241423e49e5e05816be7252e7cb
meng.yue <yebanhuakai@qq.com>
2017-10-19 12:55:55 +0800

    Add Annotation

--- NSRegularExpressionGetValues/main.m
+++ NSRegularExpressionGetValues/main.m
@@ -13,13 +13,13 @@ static NSString *const Regex_Stock = @"^http://stock\\.jrj\\.com\\.cn/(share,|co
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
 
         NSString *url = @"http://stock.jrj.com.cn/share,sh000001.shtml";
         BOOL isMatch = [NSRegularExpression isMatchRegularExpression:Regex_Stock checkString:url];
         NSLog(@"%@", isMatch ? @"YES" : @"NO"); //YES
         
-        NSArray *parts = [NSRegularExpression arrayOfCheckStringWithRegularExpression:@"(share,|cominfo/default_).*?(sh|sz)?(\\d{6})" checkString:url];
+        //你在表达式中用"()"的部分将被取出放入数组中，个数取决于"()"的数量
+        NSArray<NSString *> *parts = [NSRegularExpression arrayOfCheckStringWithRegularExpression:@"(share,|cominfo/default_).*?(sh|sz)?(\\d{6})" checkString:url];
         NSLog(@"%@", parts); //[@"share,", @"sh", @"000001"]
         
     }
